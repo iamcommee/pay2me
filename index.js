f01524f4f1b730a0fdd82d23d0903df161182f5a
Chaloempong Budkrajang <commee@buzzwoo.de>
2022-04-19 22:03:49 +0700

    Update response

--- index.js
+++ index.js
@@ -176,7 +176,7 @@ async function slackActivity(req, res, next) {
                     "type": "section",
                     "text": {
                         "type": "mrkdwn",
-                        "text": `${message}`
+                        "text": `${message} \n [QR Code Link](${imageUrl})`
                     },
                     "accessory": {
                         "type": "image",
